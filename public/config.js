391c98db08d5e31c16e6bba2f7b7b153f34bfe20
Arun John <arjkizhakkedathu@gmail.com>
2022-02-01 12:14:40 +0530

    [Update]

--- public/config.js
+++ public/config.js
@@ -1,19 +1,32 @@
+// turnConfig = {
+//   iceServers: [{
+//       urls: [
+//         "stun:stun.l.google.com:19302",
+//         "stun:stun1.l.google.com:19302",
+//         "stun:stun2.l.google.com:19302",
+//         "stun:stun3.l.google.com:19302",
+//         "stun:stun4.l.google.com:19302"
+//       ]
+//     },
+//     {
+//       username: "test",
+//       credential: "test",
+//       urls: [
+//         "turn:34.203.86.136:3478"
+//       ]
+//     }
+//   ]
+// }
+//
+
+
+
 turnConfig = {
-    iceServers: [
-    {
-      urls: [
-      "stun:stun.l.google.com:19302",
-      "stun:stun1.l.google.com:19302",
-      "stun:stun2.l.google.com:19302",
-      "stun:stun3.l.google.com:19302",
-      "stun:stun4.l.google.com:19302" ]
-    },
-    {
-      username: "test",
-      credential: "test",
-      urls: [
-        "turn:34.203.86.136:3478"
-       ]
-     }
-   ]
+  iceServers: [{
+    urls: ["stun:global.stun.twilio.com:3478"]
+  }, {
+    username: "22b3838d923a0f30d9ea922094c6f6b180d477a6d85e3440e8e7a0534ec42095",
+    credential: "Mz5S7MAKEKf2cxzObg23xX0a2KxSsODqBGK+PBSx1M0=",
+    urls: ["turn:global.turn.twilio.com:3478"]
+  }]
 }
